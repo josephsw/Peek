e7aa1dc58c871dc0b874214d18337c74a8987120
Joseph Wang <j.wang@meta-view.com>
2014-10-02 13:28:38 -0700

    Decoupled frame rate, multiple take support, audio support, etc
    * Adjustable mesh frame rate (for cameras that export at a different
    framerate)
    * Decoupled mesh from the application frame rate
    * Ability to have multiple takes in one sequence
    * Takes can play progressively
    * Start frame number can be adjusted
    * Sync video to audio track (if using actual frame numbers when
    exporting, it will sync with original audio from the video)
    * Mesh visibility can be toggled (call playMesh externally)

--- Example_Unity_Project/Assets/RunFrames.cs
+++ Example_Unity_Project/Assets/RunFrames.cs
@@ -1,34 +1,87 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
+public class RunFrames : MonoBehaviour
+{
 
-public class RunFrames : MonoBehaviour {
+    #region structs
 
-	// The number of frames in your animation
-	public int NUM_FRAMES;
+    [System.Serializable]
+    public struct FramesFolder              // Struct to store the information about a specific take
+    {
+        public string folderName;           // Directory (Resources/<FOLDER>) that the frames are located - must have "/" at the end
+        public string framePrefix;          // The prefix of the frame names ("save." by default from RGDB Toolkit)
+        public int startIndex;              // The starting frame number of your sequence
+        public int numberOfFrames;          // How long your sequence is
+        public int frameRate;               // The frame rate that your sequence was captured at (RGB camera)
+        public AudioClip audioTrack;        // The accompanying audio clip (frame numbers should match)
+    }
 
-	// The directory (Resources/<FOLDER>) where your frames are stored
-	//    Be sure to include a "/" at the end!
-	public string FOLDER;
+    #endregion
 
-	// All frame filenames must begin with this naming convention
-	//    If nothing is provided, defaults to RGBD Toolkit export
-	public string FRAME_NAME_CONV;
-		
-	private ArrayList frames = new ArrayList();
-	private int frameNum;
-	
-	//Loads all frames into RAM from file structure
-	private void LoadImages()
+    #region inspector
+    
+    private bool _playMesh = true;
+    public bool playMesh                    // Toggle to display/play hologram
+    {
+        get { return _playMesh; }
+        set
+        {
+            gameObject.renderer.enabled = value;
+            _playMesh = value;
+        }
+    }
+
+    public bool loopSequence = false;       // Loop the sequence back to 0 once it is finished
+    public bool continuousPlay = false;     // Starts playing the next take in the sequence once current one is finished
+
+    public List<FramesFolder> sequences;    // The list of takes (displayed in the inspector)
+
+    //Audio section
+    public AudioSource videoSound;          // Audio Source on the GameObject should be dragged hre
+    private int videoSoundStartIndex;       // Global for finding where to start the audio clip
+    public int soundSampleFrequency = 48000;// Frequency of the sound clip in Hz
+    public int sampleSyncFrequency = 48000; // How often to sync the image to the sound (in Hz - eg. 48000 is every second)
+    public int sampleSyncFrames = 30;       // How many frames exist per sync period (30fps = 30 at 48000 sync freq)
+    public int frameCompensation = 1;       // How much to increment the frame number beyond the calculated sync point
+
+    #endregion
+
+    #region take variables
+
+    private int numberOfFrames;              // The number of frames in the current take
+    private int frameRate = 30;              // The frame rate of the current take
+
+    //Frame storage
+    private List<Texture2D[]> frameArrays = new List<Texture2D[]>();    // Store for the frames to be loaded into memory at Start
+    private Texture2D[] currFrameArray;     // Pointer to the current take frames
+    public int sequenceIndex = 0;           // The index of which take to use from the take list (sequences)
+	private int frameNum = 0;               // The current frame number
+
+    #endregion
+
+    #region runtime variables
+
+    private float timeAtStart;              // Unity Time.Time at the start of playing the sequence (for frame calculation)
+    private int nextSampleSync = 1;         // Counter to check when to next sync audio/video (based on sampleSyncFrequency)
+
+    #endregion
+
+    // Loads all frames into RAM from the file structure
+	private void LoadImages(int sequenceToRead, string folderName, string framePrefix, int startIndex, int numberOfFrames)
 	{
-		if (FRAME_NAME_CONV.Equals ("")) 
+		if (framePrefix.Equals ("")) 
 		{
-			FRAME_NAME_CONV = @"save.";
+			framePrefix = @"save.";
 		}
+
+        //Add new array to the list of frame arrays
+        frameArrays.Add(new Texture2D[numberOfFrames+2]);
 	     
-	    for (int i=0; i < NUM_FRAMES; i++)
+	    for (int i=startIndex-1; i < numberOfFrames+startIndex; i++)
 	    {
-			// This mess prepends the correct amount of zeroes to the current frame number
+			// Prepends the correct amount of zeroes to the current frame number
 	    	string index = "";
 	    	float logIdx = Mathf.Log10(i+1);
 	    	
@@ -45,38 +98,119 @@ public class RunFrames : MonoBehaviour {
 			else Debug.Log("Too many frames in animation!");
 	    	
 	    	index += (i+1);
-	     
-			// This is deprecated functionality for compiling to mobile. We'll fix this soon
-
-			//string pathPrefix = @"file://";
-			//string fileSuffix = @".png";
-			//string pathImageAssets = Application.dataPath + "/depthPNGs";
-			//string fullFilename = pathPrefix + pathImageAssets + filename + indexSuffix + fileSuffix;
-			//WWW www = new WWW(fullFilename);
-	    	//Texture2D texTmp = new Texture2D(512, 1024, TextureFormat.DXT5, false);
-	    	//LoadImageIntoTexture compresses JPGs by DXT1 and PNGs by DXT5
-	    	//www.LoadImageIntoTexture(texTmp);
 
 			// Create a Texture2D from the specified image file
-			Texture2D frame = Resources.Load<Texture2D>(FOLDER + FRAME_NAME_CONV + index);
+			Texture2D frame = Resources.Load<Texture2D>(folderName + framePrefix + index);
 	     
-			// Add this frame to the ArrayList in RAM
-	    	frames.Add(frame);
+			// Add this frame to the sequences list in RAM
+            frameArrays[sequenceToRead][i + 1 - startIndex] = frame;
 		}
 	}
+
+    // Loads the take, including the texture PNGs and the global variables for frame rate, etc.
+    public void LoadSequence(int index)
+    {
+        sequenceIndex = index;
+
+        // Calculate the time sample that the audio clip should start at, based on the start frame number
+        videoSoundStartIndex = (int)((float)(sequences[sequenceIndex].startIndex - 1) / sequences[sequenceIndex].frameRate * soundSampleFrequency);
+        frameRate = sequences[sequenceIndex].frameRate;
+        numberOfFrames = sequences[sequenceIndex].numberOfFrames;
+        if (videoSound != null && sequences[sequenceIndex].audioTrack != null)
+        {
+            videoSound.clip = sequences[sequenceIndex].audioTrack;
+        }
+        currFrameArray = frameArrays[sequenceIndex];
+        frameNum = 0;
+    }
 	
-	// Use this for initialization
 	void Start () {
-		QualitySettings.vSyncCount = 0;
-		Application.targetFrameRate = 24;
-		LoadImages();
-		frameNum = 0;
+		//QualitySettings.vSyncCount = 0;
+		//Application.targetFrameRate = frameRate;
+
+        if (sequences.Count > 0)
+        {
+            // Load all the takes into RAM
+            for (int i = 0; i < sequences.Count; ++i)
+            {
+                LoadImages(i, sequences[i].folderName, sequences[i].framePrefix, sequences[i].startIndex, sequences[i].numberOfFrames);
+            }
+
+            //Load the global variables for the sequence
+            LoadSequence(0);
+        }
 	}
 	
-	// Set the texture of the model and increment the frame counter
 	void Update () {
-		Texture2D tex = (Texture2D)frames[frameNum];
-		renderer.material.SetTexture("_MainTex", tex);
-		frameNum = (frameNum + 1) % NUM_FRAMES;
+        if (_playMesh || Input.GetKeyDown(KeyCode.A))
+        {
+            // Reset start time if at frame 0
+            if (frameNum == 0)
+            {
+                timeAtStart = Time.time;
+            }
+
+            // If restarting the loop, reset the audio clip
+            if (frameNum == 0 && videoSound != null)
+            {
+                videoSound.Stop();
+                videoSound.timeSamples = videoSoundStartIndex;
+                videoSound.Play();
+                nextSampleSync = 1;
+            }
+
+            // Calculate which frame to display (to decouple from the game's frame rate)
+            float timeDiff = Time.time - timeAtStart;
+            int frameToShow = (int)(timeDiff * frameRate);
+            frameNum = frameToShow;
+
+            frameNum = Mathf.Min(frameNum, numberOfFrames - 1);     // Limit frame number to the last frame
+
+            // Display the texture on the mesh
+            Texture2D tex = currFrameArray[frameNum];
+            renderer.material.SetTexture("_MainTex", tex);
+
+            //frameNum = (frameNum + 1) % numberOfFrames;           // To perform the frame looping
+            frameNum = frameNum + 1; // Increment frame
+
+            // Move onto next take in sequence if at the end of this take, or triggered by keypress
+            if (frameNum >= numberOfFrames || Input.GetKeyDown(KeyCode.A))
+            {
+                frameNum = numberOfFrames - 1;                      // Load final frame
+                tex = currFrameArray[frameNum];
+                renderer.material.SetTexture("_MainTex", tex);
+
+                //LoadSequence((sequenceIndex + 1) % sequences.Count);  // Wrap around after last take in sequence
+                sequenceIndex += 1;
+                if (sequenceIndex < sequences.Count || loopSequence)    // If not on last take of sequence, cue next take
+                {
+                    LoadSequence(sequenceIndex % sequences.Count);
+                    frameNum = 0;
+                }
+
+                if (!continuousPlay)
+                {
+                    _playMesh = false;                              // Freeze
+                }
+                return;
+            }
+
+            // Check if visual has fallen behind audio, catch up by changing the frame number (sync video to audio)
+            if (frameNum != 0 && videoSound != null && videoSound.timeSamples > videoSoundStartIndex + nextSampleSync * sampleSyncFrequency)
+            {
+
+                if (frameNum + 1 < nextSampleSync * sampleSyncFrames)
+                {
+                    //Debug.Log("Fallen behind: " + videoSound.timeSamples);
+                    frameNum = (nextSampleSync * sampleSyncFrames) + frameCompensation;
+                    timeAtStart = Time.time - ((float)frameNum / frameRate);
+                    if (frameNum > numberOfFrames)
+                    {
+                        frameNum = 0;
+                    }
+                }
+                nextSampleSync += 1;
+            }
+        }
 	}
 }
--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 Peek
 ============
 
+Forked from Peek by Andy Biar (andybiar) and buenopolis.
+
+Features added by josephsw:
+* Adjustable mesh frame rate (for cameras that export at a different framerate)
+* Decoupled mesh from the application frame rate
+* Ability to have multiple takes in one sequence
+* Takes can play progressively
+* Start frame number can be adjusted
+* Sync video to audio track (if using actual frame numbers when exporting, it will sync with original audio from the video)
+* Mesh visibility can be toggled (call playMesh externally)
+
+============
+
 Peek is a system of creating augmented reality content using the [Unity3D Game Engine](http://unity3d.com). Check it out on [Vimeo](http://vimeo.com/andybiar/peek).
 
 Peek consists of several components, one of which is the RGBDShader, a Unity shader designed to render 3D models from image files. The [RGBD Toolkit](http://rgbdtoolkit.com) can export image files of the correct format, allowing you a complete workflow from filming animations with the Kinect to rendering them in Unity.
